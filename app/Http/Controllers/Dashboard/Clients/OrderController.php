120661cf1c49d9d77c7b065f765dcefd8e502717
Hossam Hamed <hossamhamed668@gmail.com>
2022-01-03 13:31:28 -0800

    initial commit

---
+++ app/Http/Controllers/Dashboard/Clients/OrderController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard\Clients;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Order;
+use App\Models\Client;
+
+class OrderController extends Controller
+{
+    public function index(){
+
+    }
+    
+    public function create(Client $client){
+
+    }
+
+    public function store(Request $request,Client $client){
+
+    }
+
+    public function edit(Client $client,Order $order){
+
+    }
+
+    public function update(Request $request,Client $client,Order $order){
+
+    }
+
+    public function destroy(Client $client,Order $order){
+        
+    }
+}
